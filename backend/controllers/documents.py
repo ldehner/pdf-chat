6f09302664063bbdc5e662cd37fafa9a3585b93c
saulex16 <scastaneda@itba.edu.ar>
2024-09-22 13:55:54 +0200

    wip: docs processing and model

--- backend/controllers/documents.py
+++ backend/controllers/documents.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, UploadFile, File, BackgroundTasks, Response, status
 from typing_extensions import Annotated
+from services.documents import DocumentsService
 
 router = APIRouter(
     prefix="/documents",
@@ -7,12 +8,18 @@ router = APIRouter(
 )
 
 @router.get("/")
-async def get_documents():
+async def get_documents() -> list[UploadFile]:
     pass
 
 @router.post("/")
-async def create_document():
-    pass
+async def create_document(background_tasks: BackgroundTasks, file: UploadFile = File(...)) -> Response:
+    document = await DocumentsService.add_document(file)
+    background_tasks.add_task(DocumentsService.generate_embeddings, document)
+
+    return Response(
+        status_code=status.HTTP_201_CREATED,
+        headers={"Location": f"/documents/{document.id}"}
+    )
 
 @router.get("/{id}")
 async def get_document(id: Annotated[int, Path(ge=1)]):
